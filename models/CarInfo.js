5c1d56b6b0ff8dac3f6e7b1c898386ca4c61360c
Tolga Secme <tolgasecme@icloud.com>
2021-06-09 21:45:46 -0600

    api search is working

--- models/CarInfo.js
+++ models/CarInfo.js
@@ -1,17 +1,7 @@
 // models: year, milage, carmodel, vin
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-
-module.exports = (mongoose) => {
-  const carSchema = new mongoose.Schema({
-    year: {
-      type: Number,
-    },
-    mileage: {
-      type: Number,
-      required: [true, "can't be blank"],
-    },
+  const carSchema = new Schema({
     carModel: {
       type: String,
       required: [true, "can't be blank"],
@@ -20,8 +10,14 @@ module.exports = (mongoose) => {
       type: String,
       required: [true, "can't be blank"],
     },
-  });
-  const Car = mongoose.model("Car", carSchema);
-  return Car;
-};
-
+    price: {
+      type: String,
+      required: [true, "can't be blank"],
+    },
+    mileage: {
+      type: Number,
+      required: [true, "can't be blank"],
+    }
+});
+const CarInfo = mongoose.model("CarInfo", carSchema);
+module.exports = CarInfo;
