6db6939b3e458810d34da30218c135c0dabaf7a1
Tolga Secme <tolgasecme@icloud.com>
2021-06-10 15:45:45 -0600

    saved page out of the auth and aboutmerners page fixed

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -3,13 +3,9 @@ import About from '../components/About';
 function AboutPage (){
    
   return(
-      <div className="container">
-        <div className="row">
           <div className="col-sm-12">
           <About />
           </div>
-        </div>
-      </div>
   )
 }
 export default AboutPage;
