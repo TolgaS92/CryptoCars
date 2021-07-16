8446281b2a8c4a626b850b11e585c5f97a954953
Tolga Secme <tolgasecme@icloud.com>
2021-07-16 15:33:28 -0600

    main page fixed with login only says check value or login// saved page useState{} changed to useState[]

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,32 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
 import './style.css';
 import Crypto from '../components/Crypto'
 // import Signup from '../components/Signup';
 import UserStory from '../components/UserStory';
+import decode from 'jwt-decode';
+
 
 function Main () {
+    const location = useLocation();
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    
+    useEffect(() => {
+        const token = user?.token;
+        /*  JWT */
+        if(token) {
+            const decodedToken = decode(token);
+            if(decodedToken.exp * 1000 < new Date().getTime());
+        }
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location])
+
     return(
         <div>
         <section className="main-top grid lg:grid-cols-2 md:grid-cols-2 gap-4 grid-cols-1">
-            <div className="container">
-                <h1 className="text-warning" >
-                    Get Your Car Value in Crypto
-                </h1>
-                <h2 className="text-warning">
-                    By using your VIN number!
-                </h2>
-                <a className="btn btn-dark btn-outline-warning px-3" href="/search">
-                    Check Value
-                </a>
-                </div>
-                <div className="container">
-                <h2 className="text-warning">Create an account or Login to save your search history</h2>
-                <span> </span>
-                <a className="btn btn-dark btn-outline-warning px-3" href="/login">
-                    Log In
-                </a>
-                </div>
-           
+                { user ? (
+                    <div className="container">
+                    <h1 className="text-warning" >
+                        Get Your Car Value in Crypto
+                    </h1>
+                    <h2 className="text-warning">
+                        By using your VIN number!
+                    </h2>
+                    <a className="btn btn-dark btn-outline-warning px-3" href="/search">
+                        Check Value
+                    </a>
+                    </div>
+                    ) : (
+                        <div className="container">
+                            <h2 className="text-warning">Create an account or Login to save your search history</h2>
+                            <span> </span>
+                            <a className="btn btn-dark btn-outline-warning px-3" href="/auth">
+                            Log In
+                            </a>
+                        </div>
+                )}           
         </section>
         <UserStory />
         <Crypto />
